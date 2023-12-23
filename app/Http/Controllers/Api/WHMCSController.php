9a3739c55852fc4d114a273c89bac743ebb91703
RyderAsking <clasherlodt@gmail.com>
2023-12-23 20:17:32 +0530

    basic methods

---
+++ app/Http/Controllers/Api/WHMCSController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WHMCSController extends Controller
+{
+    //
+    public function CreateAccount()
+    {
+        return true;
+    }
+
+    public function SuspendAccount()
+    {
+        return true;
+    }
+
+    public function UnsuspendAccount()
+    {
+        return true;
+    }
+
+    public function TerminateAccount()
+    {
+        return true;
+    }
+
+    public function ChangePassword()
+    {
+        return true;
+    }
+}
